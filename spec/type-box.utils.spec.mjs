4901968e74e238d00384980f5e616c07a40f9674
Richard Hunter <richard@richardhunter.co.uk>
2024-02-15 18:38:08 +0000

    removing Jasmine

--- spec/type-box.utils.spec.mjs
+++
@@ -1,7 +0,0 @@
-import { createPropertyEl } from "../src/type-box/create-property-el.js";
-
-describe("createPropertyEl()", () => {
-  it("should ...", () => {
-    expect(true).toBe(true);
-  });
-});
